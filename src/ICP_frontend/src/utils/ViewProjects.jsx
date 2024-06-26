62544d3cde0358bc7fc029fbb4111e3598fc05a8
sukhesh-vsk <sukhesh.vsk2005@gmail.com>
2024-06-26 22:58:37 +0530

    Created View Projects component

--- src/ICP_frontend/src/utils/ViewProjects.jsx
+++ src/ICP_frontend/src/utils/ViewProjects.jsx
@@ -23,13 +23,19 @@ export const ViewProjects = () => {
                         <p class="mb-2 text-sm font-normal text-gray-700 dark:text-gray-400">{data.description}</p>
                         <button href="#" className={`flex w-24 items-center px-3 py-1 text-sm font-medium text-center text-prime-text ring-1 ring-prime-text rounded-lg focus:outline-none dark:bg-prime`} onClick={() => toggleLike(index)}>
                             {liked[index] ? 'Liked' : 'Like'} &nbsp;
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="thumbs-up" fill='red' className='w-5 h-5 ms-1'>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="Thumbsup" className='w-5 h-5 ms-1'>
+                                <path d="M5.5 10.5h-5a.5.5 0 0 0-.5.5v12a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 6 23V11a.5.5 0 0 0-.5-.5zM24 11c0-1.378-1.122-2.5-2.5-2.5h-6.295c.305-.929.795-2.632.795-4 0-2.169-1.843-4-3-4-1.039 0-1.781.584-1.813.609A.504.504 0 0 0 11 1.5v3.39l-2.88 6.241-.843.421A.503.503 0 0 0 7 12v9c0 .133.053.26.146.354.749.748 2.429 1.146 3.354 1.146h9.25a2.252 2.252 0 0 0 2.015-3.25 2.24 2.24 0 0 0 1-3 2.24 2.24 0 0 0 1.235-2c0-.6-.239-1.161-.65-1.575.415-.452.65-1.044.65-1.675z" 
+                                    fill={(liked[index]) ? '#025514' : 'none'} stroke={(liked[index]) ? 'none' : '#025514'}  strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                    class="color000000 svgShape">
+                                </path>
+                            </svg>
+                            {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="thumbs-up" fill='red' className='w-5 h-5 ms-1'>
                                 <path d="M7.5 23.501h-7a.5.5 0 0 1-.5-.5v-12a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v12a.5.5 0 0 1-.5.5zm-6.5-1h6v-11H1v11z"
                                     fill='#025514'></path>
                                 <path d="M19.75 22.501H10.5c-.925 0-2.605-.398-3.354-1.146a.5.5 0 0 1 .707-.707c.473.473 1.834.854 2.646.854h9.25c.689 0 1.25-.561 1.25-1.25 0-.344-.14-.666-.395-.907a.499.499 0 0 1 .286-.859A1.249 1.249 0 0 0 22 17.251c0-.344-.14-.666-.395-.907a.499.499 0 0 1 .286-.859A1.247 1.247 0 0 0 23 14.251c0-.466-.262-.89-.684-1.106a.497.497 0 0 1-.034-.87c.449-.278.718-.754.718-1.274 0-.827-.673-1.5-1.5-1.5h-7a.502.502 0 0 1-.468-.677c.01-.027.968-2.564.968-4.323 0-1.637-1.452-3-2-3-.431 0-.792.157-1 .272v3.228a.501.501 0 0 1-.046.209l-3 6.5a.506.506 0 0 1-.23.238l-1 .5a.5.5 0 0 1-.447-.895l.843-.421L11 4.891v-3.39c0-.152.069-.295.188-.391A3.13 3.13 0 0 1 13 .501c1.157 0 3 1.831 3 4 0 1.368-.49 3.071-.795 4H21.5c1.378 0 2.5 1.122 2.5 2.5 0 .631-.235 1.223-.65 1.675.411.413.65.974.65 1.575a2.24 2.24 0 0 1-1.235 2 2.24 2.24 0 0 1-1 3 2.253 2.253 0 0 1-2.015 3.25z"
-                                    fill='#025514' class="color000000 svgShape">
+                                    fill='#025514'>
                                 </path>
-                            </svg>
+                            </svg> */}
                         </button>
                     </div>
                 </a>
